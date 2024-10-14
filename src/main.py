6b82577d0f3bb566af0edf0e9a708a371429f433
Alexandr Khorev <horev149@gmail.com>
2024-10-14 14:15:48 +0500

    Add new logic from calculate.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
+# TODO: Add new functionality from calculate.py
 import streamlit as st
 from openpyxl import load_workbook
+
 from core.extractor.extractor import extract_data_from_sheet
+from core.calculate import MainCalculation
+
 
 st.title(
     'Calculation of a capped pile'
@@ -21,6 +25,15 @@ if file:
     st.toast('File successfully uploaded', icon='🔥')
 
     workbook = load_workbook(file, data_only=True)
-    data = extract_data_from_sheet(workbook['PileData'], 'PileData')
+    pile_data = extract_data_from_sheet(workbook['PileData'], 'PileData')
+    load_data = extract_data_from_sheet(workbook['LoadData'], 'LoadData')
+    foundation_data = extract_data_from_sheet(workbook['FoundationData'], 'FoundationData')
 
-    st.sidebar.markdown(f"{data}")
+    result = MainCalculation().calculate(pile_data, load_data, foundation_data)
+
+    if result.get('is_passed'):
+        st.success('Calculation successful')
+        st.json(result)
+    else:
+        st.error('Calculation failed')
+        st.json(result)
